da56801af9d3e96fcaca543d3ecc4c1b72bdd367
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2015-10-21 02:59:05 +0000

    Fix serve command

--- roles/nginx/templates/serve.sh
+++ roles/nginx/templates/serve.sh
@@ -8,9 +8,10 @@
 #
 
 block="server {
-    listen 80;
+    listen ${3:-80};
+    listen ${4:-443} ssl;
     server_name $1;
-    root $2;
+    root \"$2\";
 
     index index.html index.htm index.php;
 
@@ -23,25 +24,33 @@ block="server {
     location = /favicon.ico { access_log off; log_not_found off; }
     location = /robots.txt  { access_log off; log_not_found off; }
 
-    access_log /var/log/nginx/$1-access.log combined;
+    access_log off;
     error_log  /var/log/nginx/$1-error.log error;
 
-    error_page 404 /index.php;
-
     sendfile off;
+
+    client_max_body_size 100m;
 
     location ~ \.php$ {
         fastcgi_split_path_info ^(.+\.php)(/.+)$;
         fastcgi_pass unix:/var/run/php5-fpm.sock;
         fastcgi_index index.php;
         include fastcgi_params;
+        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+        fastcgi_intercept_errors off;
+        fastcgi_buffer_size 16k;
+        fastcgi_buffers 4 16k;
+        fastcgi_connect_timeout 300;
+        fastcgi_send_timeout 300;
+        fastcgi_read_timeout 300;
     }
 
     location ~ /\.ht {
         deny all;
     }
 
-    include {{ nginx_config_directory }}/h5bp/basic.conf;
+    ssl_certificate     /etc/nginx/ssl/$1.crt;
+    ssl_certificate_key /etc/nginx/ssl/$1.key;
 }
 "
 
